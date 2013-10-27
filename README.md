290fbb8e86f35861275fe0fa9e01bac602917f29
zensh <admin@zensh.com>
2013-10-27 09:57:38 +0800

    new loader
    Signed-off-by: zensh <admin@zensh.com>

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 reserveLoad.js
 ===========
 
-**Asyncronous JavaScript/CSS loader and dependency manager, and load JavaScript with reserve URL.**
+**Dynamic JavaScript/CSS loader, and load JavaScript with reserve URL.**
 
-reserveLoad.js 能从多个备用网址异步加载JS文件。比如使用Google CDN资源时，有时可能因为网络问题Google CDN不可用，就自动从备用的Sina CDN加载，如果仍然连接失败，就从服务器自身加载...
+reserveLoad.js 能从多个备用网址逐步加载JS文件。比如使用Google CDN资源时，有时可能因为网络问题Google CDN不可用，就自动从备用的Sina CDN加载，如果仍然连接失败，就从服务器自身加载...
 
 可以设定多个JS文件及他们各自的备用加载网址，当所有JS文件加载完毕，会自动运行回调函数。
-
-还可设定同时异步加载或者按照顺序逐步加载。
 
 ### Browser Support
 ---------------
@@ -33,8 +31,9 @@ reserveLoad.js 能从多个备用网址异步加载JS文件。比如使用Google
             reserveLoad(
                 ['mainUrl/jquery.js', 'reserveUrl1/jquery.js', 'reserveUrl2/jquery.js', 'jQuery'],
                 ['mainUrl/angular.js', 'reserveUrl1/angular.js', 'reserveUrl2/angular.js', 'angular'],
-                function () {
+                function (errUrl, jq, ng) {
                     alert('synchronous loaded!');
+                    console.log(errUrl, jq, ng);
                 });
             </script>
         </body>
@@ -43,37 +42,11 @@ reserveLoad.js 能从多个备用网址异步加载JS文件。比如使用Google
 
 ### Exhaustive list of ways to use reserveLoad.js
 
-synchronous, load by order
-
     reserveLoad('foo1.js', 'foo2.js', 'foo3.js', 'foo4.js', ...);
 
-asynchronous, no order
-
-    reserveLoad.async('foo1.js', 'foo2.js', 'foo3.js', 'foo4.js', ...);
-
-synchronous, load by order, run allLoadedCallBack after all js loaded.
-
-    reserveLoad('foo1.js', 'foo2.js', 'foo3.js', 'foo4.js' ...,  function allLoadedCallBack() {});
-
-asynchronous, no order, run allLoadedCallBack after all js loaded.
-
-    reserveLoad.async('foo1.js', 'foo2.js', 'foo3.js', 'foo4.js' ..., allLoadedCallBack);
-
-synchronous, load by order, with some reserve url, run allLoadedCallBack after all js loaded.when load with reserve url, a method in js must be declared to detect js is loaded or false, such as 'methodInFoo1'
+    reserveLoad('foo1.js', 'foo2.js', 'foo3.js', 'foo4.js' ...,  function (err, foo1, foo2, ...) {});
 
     reserveLoad(
         ['url1/foo1.js', 'url2/foo1.js', 'url3/foo1.js',..., 'methodInFoo1'],
         ['url1/foo2.js', 'url2/foo2.js', 'url3/foo2.js',..., 'methodInFoo2'],
-        ..., allLoadedCallBack);
-
-asynchronous, no order, with some reserve url, run allLoadedCallBack after all js loaded.'jquery' use to detect whether 'jquery.js' load success or false, 'angular'  is also.
-
-    reserveLoad.async(
-        ['url1/jquery.js', 'url2/jquery.js', 'url3/jquery.js',..., 'jQuery'],
-        ['url1/jquery-ui.js', 'url2/jquery-ui.js', 'url3/jquery-ui.js',..., 'jQuery.ui'],
-        ['url1/angular.js', 'url2/angular.js', 'url3/angular.js',..., 'angular'],
-        ..., allLoadedCallBack);
-
-
-
-
+        ..., callBack);
--- reserveLoad.js
+++ reserveLoad.js
@@ -1,138 +1,161 @@
 'use strict';
 
 /*!
- * reserveLoad.js, version 0.3.1, 2013/08/15
- * Asyncronous JavaScript/CSS loader and dependency manager, and load JavaScript with reserve URL.
+ * reserveLoad.js, version 0.4.0, 2013/10/27
+ * Dynamic JavaScript/CSS loader, and load JavaScript with reserve URL.
  * https://github.com/zensh/reserveLoad.js
  * (c) admin@zensh.com 2013
  * License: MIT
  */
 
 (function (global) {
+  var doc = global.document,
+    _define = global.define,
+    location = doc.location,
+    head = doc.getElementsByTagName('head')[0] || doc.documentElement,
+    baseElement = head.getElementsByTagName('base')[0],
+    slice = [].slice,
+    validCSS = /\.css(?:\?|$)/i,
+    validState = /^(?:loaded|complete|undefined)$/,
+    currentlyAddingScript = null,
+    scripts = {};
 
-    var doc = global.document,
-        location = doc.location,
-        head = doc.getElementsByTagName('head')[0] || doc.documentElemen,
-        baseElement = head.getElementsByTagName('base')[0],
-        slice = Array.prototype.slice,
-        validCSS = /\.css(?:\?|$)/i,
-        validState = /^(?:loaded|complete|undefined)$/;
+  var reserveLoad = global.reserveLoad = function () {
+    startLoad(slice.call(arguments));
+  };
+  reserveLoad.version = 'v0.4.0';
+  global.define = function (name, deps, callback) {
+    var fn = isFunction(callback) ? callback : isFunction(deps) ? deps : isFunction(name) ? name : noop;
+    if (currentlyAddingScript) {
+      scripts[currentlyAddingScript] = fn();
+      currentlyAddingScript = null;
+    } else if (isFunction(_define)) {
+      _define(name, deps, callback);
+    }
+  };
 
-    var reserveLoad = global.reserveLoad = function () {
-        startLoad(slice.call(arguments));
+  function noop() {}
+
+  function isArray(obj) {
+    return Array.isArray ? Array.isArray(obj) : Object.prototype.toString.call(obj) === '[object Array]';
+  }
+  function isFunction(fn) {
+    return typeof fn === 'function';
+  }
+  function each(array, iterator, context) {
+    for (var i = 0, l = array.length; i < l; i++) {
+      iterator.call(context, array[i], i, array);
+    }
+  }
+  function eachThen(array, iterator, context) {
+    var i = -1,
+      end = array.length - 1;
+    function next() {
+      i += 1;
+      iterator.call(context, i < end ? next : null, array[i], i, array);
+    }
+    return next();
+  }
+  function getScript(context, exports, url) {
+    var value = null;
+    if (url) {
+      if (scripts[url]) {
+        value = scripts[url];
+      } else if (isFunction(exports)) {
+        value = exports();
+        scripts[url] = value;
+      } else if (exports && typeof exports === 'string') {
+        value = context || global;
+        each(exports.split('.'), function (part) {
+          if (value) {
+            value = value[part];
+          }
+        });
+        scripts[url] = value;
+      }
+    }
+    return value;
+  }
+  function request(url, exports, callback) {
+    var isCSS = validCSS.test(url),
+      node = doc.createElement(isCSS ? 'link' : 'script'),
+      success = isCSS || !exports;
+
+    if (!url || (!isCSS && getScript(global, exports, url))) {
+      return;
+    }
+    node.onload = node.onerror = node.onreadystatechange = function () {
+      if (validState.test(node.readyState + '')) {
+        if (!isCSS) {
+          head.removeChild(node);
+        }
+        node = node.onload = node.onerror = node.onreadystatechange = null;
+        callback(success || !! getScript(global, exports, url));
+      }
     };
-    reserveLoad.async = function () {
-        startLoad(slice.call(arguments), true);
-    };
-    reserveLoad.version = '0.3.1';
-
-    function isArray(obj) {
-        return Array.isArray ? Array.isArray(obj) : Object.prototype.toString.call(obj) === '[object Array]';
+    if (isCSS) {
+      node.rel = 'stylesheet';
+      node.href = url;
+    } else {
+      node.async = 1;
+      node.src = url;
     }
+    currentlyAddingScript = url;
+    head[baseElement ? 'insertBefore' : 'appendChild'](node, baseElement);
+  }
 
-    function each(array, iterator, context) {
-        for (var i = 0, l = array.length; i < l; i++) {
-            iterator.call(context, array[i], i, array);
-        }
+  function startLoad(list) {
+    var count = 0,
+      total = list.length,
+      scriptList = [],
+      callback = list[total - 1];
+
+    function checkLoaded(next, url) {
+      if (url || count === total) {
+        each(scriptList, function (key, index) {
+          scriptList[index] = scripts[key] || key;
+        });
+        scriptList.unshift(url);
+        return callback.apply(null, scriptList);
+      } else if (isFunction(next)) {
+        return next();
+      }
     }
-
-    function eachThen(array, iterator, context) {
-        var i = -1,
-            end = array.length - 1;
-        next();
-
-        function next() {
-            i += 1;
-            iterator.call(context, i < end ? next : null, array[i], i, array);
+    function load(array, nextLoad) {
+      function get(url, fnName, next) {
+        request(url, fnName, function (success) {
+          count += +success;
+          if (!success && next) {
+            next();
+          } else {
+            scriptList.push(url);
+            checkLoaded(nextLoad, success ? null : url);
+          }
+        });
+      }
+      if (isArray(array)) {
+        var checkFn;
+        if (array.length > 1) {
+          checkFn = array.pop();
         }
+        eachThen(array, function (next, x) {
+          get(x, checkFn, next);
+        });
+      } else if (array && typeof array === 'string') {
+        get(array, null);
+      } else {
+        count += 1;
+        checkLoaded(nextLoad, null);
+      }
     }
-
-    function parse(context, expStr) {
-        return _parse(context, isArray(expStr) ? expStr : expStr.split('.'));
-
-        function _parse(context, array) {
-            var key = array[0],
-                value = context[key],
-                type = typeof value;
-            return array.length > 1 && (type === 'object' || type === 'function') ? _parse(value, array.slice(1)) : value;
-        }
+    if (isFunction(callback)) {
+      total -= 1;
+      list.pop();
+    } else {
+      callback = noop;
     }
-
-    function request(url, fn, callback) {
-        var isCSS = validCSS.test(url),
-            node = doc.createElement(isCSS ? 'link' : 'script'),
-            success = isCSS || !fn;
-
-        node.onload = node.onerror = node.onreadystatechange = function () {
-            if (validState.test(node.readyState + '')) {
-                success = success || !! parse(global, fn);
-                if (!success) {
-                    head.removeChild(node);
-                } else {
-                    node.onload = node.onreadystatechange = null;
-                }
-                callback(success);
-            }
-        };
-        if (isCSS) {
-            node.rel = 'stylesheet';
-            node.href = url;
-        } else if (url) {
-            node.async = 1;
-            node.src = url;
-        } else {
-            return;
-        }
-        head.insertBefore(node, head.firstChild);
-    }
-
-    function startLoad(list, async) {
-        var count = 0,
-            total = list.length,
-            callback = typeof (callback = list[total - 1]) === 'function' ? (list.length = total - 1, callback) : null;
-
-        total = callback ? total - 1 : total;
-        if (async) {
-            each(list, load);
-        } else {
-            eachThen(list, function (next, x) {
-                load(x, next);
-            });
-        }
-
-        function checkLoaded(next, url) {
-            if (url || count === total) {
-                return callback && callback(url);
-            } else if (typeof next === 'function') {
-                return next();
-            }
-        }
-
-        function load(array, nextLoad) {
-            if (isArray(array)) {
-                var len = array.length - 1,
-                    fnName = len > 0 ? array[len] : null;
-                array.length = fnName === null ? len + 1 : len;
-                eachThen(array, function (next, x) {
-                    get(x, fnName, next);
-                });
-            } else if (array && typeof array === 'string') {
-                get(array, null);
-            } else {
-                count += 1;
-                checkLoaded(nextLoad, null);
-            }
-
-            function get(url, fnName, next) {
-                request(url, fnName, function (success) {
-                    count += +success;
-                    if (!success && next) {
-                        next();
-                    } else {
-                        checkLoaded(nextLoad, success ? null : url);
-                    }
-                });
-            }
-        }
-    }
+    eachThen(list, function (next, x) {
+      load(x, next);
+    });
+  }
 })(window);
